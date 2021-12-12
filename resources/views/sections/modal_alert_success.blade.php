ec716838b9a3cb2ba0b692cc687455624323022d
Jaiver Ocampo <camiso2@gmail.com>
2021-12-12 17:24:31 -0500

    format code final section all

--- resources/views/sections/modal_alert_success.blade.php
+++ resources/views/sections/modal_alert_success.blade.php
@@ -10,18 +10,16 @@
 					<!--<h1>Su participación fue recibida.</h1>
 					<h1>¡Gracias!</h1>
 					<h1># confirmacion 027</h1>-->
-					<h2 class="confirm">{{ session('alert_success') }} </h2>	
-					<h2 class="confirm">{{ session('gracias') }} </h2>	
+					<h2 class="confirm">{{ session('alert_success') }} </h2>
+					<h2 class="confirm">{{ session('gracias') }} </h2>
 					<h2 class="confirm">{{ session('code') }}</h2>
-				</div>	
+				</div>
 				<br>
 				<div class="box-a">
 					<img src="/images/a1.png" alt="" ><hr style="background-color: white">
 				</div>
 				<h3 class="sbf-title color-1 box" ><img src="/images/a2.png"></h3>
-				
 				<button type="button" class="btn btn-ttc btn-style" style="background-color: #BD3247" onclick="moverseAs()">REGISTRAR MAS RECIBOS </button>
-
 				<script type="text/javascript">
 					function moverseAs() {
 						location.hash = "#" + "form";
--- resources/views/sections/modal_statistiques.blade.php
+++ resources/views/sections/modal_statistiques.blade.php
@@ -1,21 +1,21 @@
 <!-- Modal -->
 <div class="modal fade" id="statistiques" role="dialog" data-backdrop="true">
-	<div class="modal-dialog modal-lg" >
-		<!-- Modal content-->
-		<div class="modal-content" >
+    <div class="modal-dialog modal-lg">
+        <!-- Modal content-->
+        <div class="modal-content">
 
-			<div class="modal-header">
-				<h4 class="modal-title"><b>Estadisticas del Sorteo !</b></h4>
-				<button type="button" class="close" data-dismiss="modal">&times;</button>
-			</div>
-			<div class="modal-body" style="text-align: center;">
-				<div class="card">
-					<div class="card-body">
-<div class="card-body">
-						<canvas id="myChart"></canvas>
-						<script src="chart.js"></script>
-						<script>
-							var ctx = document.getElementById('myChart').getContext('2d');
+            <div class="modal-header">
+                <h4 class="modal-title"><b>Estadisticas del Sorteo !</b></h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body" style="text-align: center;">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-body">
+                            <canvas id="myChart"></canvas>
+                            <script src="chart.js"></script>
+                            <script>
+                                var ctx = document.getElementById('myChart').getContext('2d');
 							var chart = new Chart(ctx, {
 								type: 'doughnut',
 								data:{
@@ -26,15 +26,14 @@
 										labels: ['Google Chrome','Safari','Edge','Firefox','Opera']},
 										options: {responsive: true}
 									});
-								</script>
-							</div>
-
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-primary btn-style" data-dismiss="modal">Cerrar</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                            </script>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary btn-style" data-dismiss="modal">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
