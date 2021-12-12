c865e9e99c11b0949b552090f7c9894f421d3787
Jaiver Ocampo <camiso2@gmail.com>
2021-12-12 17:11:23 -0500

    update code format welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,6 @@
     <script src="https://unpkg.com/gijgo@1.9.13/js/messages/messages.es-es.js" type="text/javascript"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-
 </head>
 <body>
     @if (session('alert_error'))
@@ -36,7 +34,6 @@
 </script>
 @include('sections.modal_alert_error')
 @endif
-
 @if (session('alert_success'))
 <script>
   $(document).ready(function()
@@ -58,11 +55,9 @@
             <div class="col-md-8">
                 @include('sections.preload')
                 <div class="card-body">
-
                     <h3 class="sbf-title color-1 box-j" ><img src="/images/a1.png"></h3>
                     <h3 class="sbf-title color-1 box" ><img src="/images/a2.png"></h3>
                     <h3 class="sbf-title color-1 box" ><img src="/images/a4.png"></h3>
-                    
                     <div class="container" style="margin-bottom: 50px;">
                         <div class="row space ">
                             <div class="col-md-5 box" >
@@ -93,40 +88,29 @@
                             </div>
                             <div class="col-md-7">
                                 <h1 class="prem-title color-3 font">3<span class="er">er</span>&nbsp;&nbsp;&nbsp;&nbsp; premio</h1>
-                                <!--<h1 class=" color-3 font-prem">Gift Cards de Econo valorados <br>en $150 (10 ganadores)</h1>-->
-
                                  <h1 class=" color-3 font-prem">Gift Cards de Econo valorados</h1>
                                 <h1 class=" color-3 font-prem">en $150 <span class="phone">(10 ganadores)</span></h1>
                             </div>
                         </div>
                     </div>
-
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-2">
                             <button type="button" class="btn btn-ttc  font" data-toggle="modal" data-target="#term">👉 Ver reglas aquí 👈</button>
-
                         </div>
                     </div>
                     <br>
                     <h3 class="scf-title color-1 box" ><img src="/images/a6.png"></h3>
                 </div>
-
                 @include('sections.modal_term')
-
                 <div class="text">
                   <div class="container" >
                     <div class="row justify-content-center">
                         <div class="col-md-12">
                             <div class="card" >
-
-                                {{--  <div class="card-header">{{ __('Register') }}</div>--}}
                                 @include('sections.preload')
                                 <div class="card-body">
-
-
                                     <form class="form-horizontal" id="form" method="POST" action="{{ route('registerReceipt') }}" enctype="multipart/form-data" onsubmit="return validacion()" autocomplete="off">
                                         {{ csrf_field() }}
-
                                         <div class="form-group row {{ $errors->has('nombres') ? ' is-invalid' : '' }}">
                                             <div class="col-md-6 acer">
                                                 <input id="name" type="text" class="form-control" name="nombres" value="{{ old('nombres') }}" required >
@@ -148,7 +132,6 @@
                                                 <label for="name" class="col-md-12 col-form-label text-md-left font">{{ __('APELLIDO') }}</label>
                                             </div>
                                         </div>
-
                                         <div class="form-group row{{ $errors->has('telefono') ? ' has-error' : '' }}">
                                             <div class="col-md-12">
                                                 <input id="phone"  type="number" onkeypress='return event.charCode >= 48 && event.charCode <= 57' class="form-control" name="telefono" value="{{ old('telefono') }}" maxlength="14" min="1" required  >
@@ -161,7 +144,6 @@
                                                 <label for="name" class="col-md-12 col-form-label text-md-left font">TELÉFONO</label>
                                             </div>
                                         </div>
-
                                         <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
                                             <div class="col-md-12">
                                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required >
@@ -174,23 +156,17 @@
                                             <label for="name" class="col-md-12 col-form-label text-md-left font">EMAIL</label>
                                         </div>
                                     </div>
-
-
-
                                     <div class="form-group row{{ $errors->has('nacimiento') ? ' has-error' : '' }}">
-
                                         <div class="col-md-4">
                                             <div class="input-group"> <span class="input-group-addon"></span>
                                                 <select id="lunch" class="selectpicker form-control" name="dia" data-live-search="true" required>
                                                     <option value="{{ old('dia')== null? '' : old('dia')}}" >{{ old('dia')== null? "Día" : old('dia')}}</option>
                                                     @for ($i = 1; $i <= 31 ; $i++)
                                                     <option value="{{ $i }}">{{ $i }}</option>
-                                                    @endfor 
+                                                    @endfor
                                                 </select>
                                             </div>
-
                                         </div>
-
                                         <div class="col-md-4">
                                             <div class="input-group"> <span class="input-group-addon"></span>
                                                 <select id="lunch" class="selectpicker form-control" name="mes" data-live-search="true" required>
@@ -210,7 +186,6 @@
                                                 </select>
                                             </div>
                                         </div>
-
                                         <div class="col-md-4">
                                             <div class="input-group"> <span class="input-group-addon"></span>
                                                 <select id="lunch" class="selectpicker form-control" name="anio" data-live-search="true" required>
@@ -218,11 +193,10 @@
                                                     <option value="{{ old('anio')== null? '' : old('anio')}}" >{{ old('anio')== null? "Año" : old('anio')}}</option>
                                                     @for ($i =  date("Y")-18; $i >= 1930 ; $i--)
                                                     <option value="{{ $i }}">{{ $i }}</option>
-                                                    @endfor 
+                                                    @endfor
 
                                                 </select>
                                             </div>
-
                                         </div>
                                         @if ($errors->has('nacimiento'))
                                         <span class="help-block">
@@ -231,16 +205,11 @@
                                         @endif
                                         <label for="name" class="col-md-12 col-form-label text-md-left font" style="margin-left: 15px;margin-bottom: 15px;">FECHA DE NACIMIENTO</label>
                                     </div>
-
-
-
-
                                     <div class="form-group row{{ $errors->has('supermercado') ? ' has-error' : '' }}">
-
                                         <div class="col-md-12">
                                             <div class="input-group"> <span class="input-group-addon"></span>
                                                 <select   id="supermercado" type="text" name="supermercado"  class="form-control" required>
-                                                    <option value="{{ old('supermercado')== null? '' : old('supermercado')}}" >{{ old('supermercado')== null? "SELECCIONA" : old('supermercado')}}</option> 
+                                                    <option value="{{ old('supermercado')== null? '' : old('supermercado')}}" >{{ old('supermercado')== null? "SELECCIONA" : old('supermercado')}}</option>
                                                     @foreach ($supermercados as $s)
                                                     <option value="{{ $s->nombres }}">{{ $s->nombres }}</option>
                                                     @endforeach
@@ -254,17 +223,14 @@
                                             <label for="name" class="col-md-12 col-form-label text-md-left font">{{ __('TIENDA ECONO EN LA QUE COMPRASTE') }}</label>
                                         </div>
                                     </div>
-
                                     <div class="form-group row{{ $errors->has('recibo') ? ' has-error' : '' }}" >
                                         <div class="col-md-12">
                                             <input id="recibo"  type="text" onkeyup="mayus(this);" class="form-control" name="recibo" value="{{ old('recibo') }}" maxlength="14" min="1" required  >
-
                                             <script type="text/javascript">
                                                 function mayus(e) {
                                                     e.value = e.value.toUpperCase();
                                                 }
                                             </script>
-
                                             @if ($errors->has('recibo'))
                                             <span class="help-block" style="">
                                                 <strong >el {{ $errors->first('recibo') }}</strong>
@@ -274,7 +240,6 @@
                                         </div>
                                     </div>
                                     <div class="form-group row frec" >
-
                                         <label for="name" class="col-md-4 col-form-label text-md-center reci font" ><br>FOTO DE RECIBO</label>
                                         <div class="col-md-8" >
                                             <label for="fper_user" class="subir" style="width: 50%;">
@@ -285,12 +250,10 @@
                                        <label for="name" class="col-md-4 col-form-label text-md-right"></label>
                                        <div class="col-md-8">
                                            <div class="fper-usu"  >
-
                                             <img src="/images/rec.jpg" id="prev_profile_user" width="100%" class="img-fluid img-thumbnail"  style="display: none;margin-bottom: 15px;" />
                                         </div>
                                     </div>
                                     <input name="fper" id="fper_user" onchange='validationProfileUser()' type="file" style='display: none;'   />
-
                                 </div>
                                 <script type="text/javascript">
                                     function validationProfileUser(){
@@ -299,8 +262,6 @@
                                         var required_file = "";
                                         var allowedExtensions = /(.jpg|.jpeg|.png|.gif|.JPG|.JPEG|.PNG|.GIF)$/i;
                                         required_file = ".jpg  .png  .gif" ;
-
-
                                         if(!allowedExtensions.exec(filePath)){
                                          swal("Upps !", "Solo puede subir la imagen requerida : " +required_file, "info");
                                          fileInput.value = '';
@@ -322,7 +283,6 @@
                                         }
                                     }
                                 }
-                                
                             </script>
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-2">
@@ -334,15 +294,8 @@
 
                             <div class="col-md-12 fat"  >
                                 <hr class="hrt">
-                   {{-- <a href="{{ route('login') }}" class="ad" onclick="preload()"><i class="fa fa-user" aria-hidden="true"></i>Login</a>
-                    <a href="" target="_blank">
-                     <i class="fa fa-facebook-f" aria-hidden="true"></i>--}}
                  </a>
-                <!-- <a href="" target="_blank">
-                     <i class="fa fa-twitter " aria-hidden="true"></i>
-                 </a>-->
              </div>
-
              <script type="text/javascript">
                function validacion() {
                 valor = document.getElementById("fper_user").value;
@@ -367,16 +320,7 @@
 </div>
 <br>
 </div>
-
-
-
-
-
-
-
 </body>
-
-
 </html>
 
 
